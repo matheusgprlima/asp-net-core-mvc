733fda77a484aaade5f35be0e960ec600b7ed5c4
Matheus Gomes <matheusgprlima@gmail.com>
2019-08-25 21:30:06 -0300

    Outras Entidades e a segunda migration

---
+++ SalesWebMVC/Models/Seller.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SalesWebMVC.Models
+{
+    public class Seller
+    {
+        public int Id { get; set; }
+        public string  Name { get; set; }
+
+        public string Email { get; set; }
+
+        public double  BaseSalary { get; set; }
+
+        public DateTime BirthDate { get; set; }
+
+        public Department Department { get; set; }
+
+        public ICollection<SalesRecord> SalesRecords { get; set; } = new List<SalesRecord>();
+
+        public Seller()
+        {
+
+        }
+
+        public Seller(int id, string name, string email, double baseSalary, DateTime birthDate, Department department)
+        {
+            Id = id;
+            Name = name;
+            Email = email;
+            BaseSalary = baseSalary;
+            BirthDate = birthDate;
+            Department = department;
+        }
+
+        public void addSales( SalesRecord salesRecord)
+        {
+            SalesRecords.Add(salesRecord);
+        }
+
+        public void removeSales(SalesRecord salesRecord)
+        {
+            SalesRecords.Remove(salesRecord);
+        }
+
+        public double TotalSales(DateTime initial, DateTime final)
+        {
+            return SalesRecords.Where(sr => sr.Date >= initial && sr.Date <= final).Sum(sr => sr.Amount);
+        }
+    }
+}
